7fd2b660d632548e5c7f7790bc81859e6a301a57
Ma_Gyul <49198230+MaGyul@users.noreply.github.com>
2024-03-23 18:58:04 +0900

    Create raidrive.user.js

---
+++ raidrive.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         ads.raidrive.com
+// @namespace    http://tampermonkey.net/
+// @version      2024-03-23
+// @description  
+// @author       You
+// @match        https://ads.raidrive.com/*
+// @match        http://ads.raidrive.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=ads.raidrive.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    location = 'about:blank';
+})();
