c6f87b385dbd05367469b18f4ec1c72a2d91d195
Ma_Gyul <49198230+MaGyul@users.noreply.github.com>
2024-06-26 18:51:30 +0900

    Update shelter.id.user.js

--- shelter.id.user.js
+++ shelter.id.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         쉘터 정확한 날자 및 시간 표시
 // @namespace    https://shelter.id/
-// @version      1.0.0
+// @version      1.0.1
 // @description  쉘터 정확한 날자 및 시간 표시
 // @author       MaGyul
 // @match        https://shelter.id/*
@@ -12,23 +12,36 @@
 
 (function() {
     'use strict';
-    if (window.updateDateTaskId) {
-        clearInterval(window.updateDateTaskId);
-    }
 
-    window.updateDateTaskId = setInterval(updateDate, 100);
+    // history onpushstate setup
+    (function(history){
+        var pushState = history.pushState;
+        history.pushState = function(state) {
+            if (typeof history.onpushstate == "function") {
+                history.onpushstate({state: state});
+            }
+            return pushState.apply(history, arguments);
+        };
+    })(window.history);
+
+    history.onpushstate = updateDate;
 
     function updateDate() {
-        let title_li = document.querySelector('.sub-txt > li:nth-child(1)');
-        if (!title_li) return;
-        let time_span = title_li.querySelector('.datetime');
-        if (!time_span) {
-            time_span = document.createElement('span');
-            time_span.classList.add('datetime');
-            let time = title_li.querySelector('time');
-            let datetime = new Date(time.getAttribute('datetime'));
-            time_span.textContent = ` (${datetime.toLocaleString()})`;
-            title_li.appendChild(time_span);
-        }
+        setTimeout(() => {
+            let title_li = document.querySelector('.sub-txt > li:nth-child(1)');
+            if (!title_li) {
+                updateDate();
+                return;
+            }
+            let time_span = title_li.querySelector('.datetime');
+            if (!time_span) {
+                time_span = document.createElement('span');
+                time_span.classList.add('datetime');
+                let time = title_li.querySelector('time');
+                let datetime = new Date(time.getAttribute('datetime'));
+                time_span.textContent = ` (${datetime.toLocaleString()})`;
+                title_li.appendChild(time_span);
+            }
+        }, 200);
     }
 })();
